7c337dd84f68768a0298e6c8f85e928044ea646e
Sanjay Tiwari <sanjay.180505@gmail.com>
2021-09-23 16:42:56 -0400

    Add Kaniko

--- demo/wip/gke_webhook_demo/command.sh
+++ demo/wip/gke_webhook_demo/command.sh
@@ -1,1 +1,3 @@
 curl -d '{"message":"ok"}' -H "Content-Type: application/json" -X POST http://localhost:31236/devops-toolkit
+#Patch Controller to work the workflow
+kubectl patch configmap workflow-controller-configmap --patch '{"data":{"containerRuntimeExecutor":"k8sapi"}}' --namespace=argo
