8af1eaa533293747a1c63e1bc6d63a7441c88503
Sanjay Tiwari <sanjay.180505@gmail.com>
2021-09-23 17:20:59 -0400

    Update for GKE

---
+++ demo/wip/gke_webhook_demo/secret.sh
@@ -0,0 +1,10 @@
+export REGISTRY_SERVER=https://index.docker.io/v1/
+export REGISTRY_USER=tiwarisanjay
+export REGISTRY_PASS=
+export REGISTRY_EMAIL=sanjay.180505@gmail.com
+#kubectl create secret docker-registry regcred \
+#	--docker-server=${REGISTRY_SERVER} \ 
+#	--docker-username=${REGISTRY_USER} \
+#	--docker-password=${REGISTRY_PASS} \
+#	--docker-email=${REGISTRY_EMAIL}
+
