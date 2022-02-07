d390632577ac1957f5c9bcc0cf1211af19b49e95
Maksym Verbovyi <maksym.verbovyi@memsource.com>
2022-02-07 17:58:19 +0100

    Update argo-events to 1.5.6 and fix RBAC

--- end_to_end.sh
+++ end_to_end.sh
@@ -10,7 +10,7 @@ validateDeployment(){
     kubectl rollout status -n ${namespace} deploy/${deployment}
     if [ $? -gt 0 ];then
         echo "Deployment ${deployment} in namespace $namespace failed please check."
-        exit 1 
+        exit 1
     fi
 }
 echo "Download ArgoCLI if not present"
@@ -26,44 +26,44 @@ if [ $? -gt 0 ];then
     mv ./argo-darwin-amd64 /usr/local/bin/argo
     # Test installation
     argo version
-    [ $? -gt 0 ] && echo "Download Argo CLI for workflow manually and run this script again." && exit 1 
-fi 
+    [ $? -gt 0 ] && echo "Download Argo CLI for workflow manually and run this script again." && exit 1
+fi
 #=================
 mydir=`pwd`
 echo "==========================================================="
 echo "Install ArgoEvent,ArgoCD,Argo Workflow"
 echo "==========================================================="
-cd prerequisite/argo-install-all
+cd argo-install-all
 ./install_all.sh
 echo "Validate for Workflow Controller Deployement....."
-validateDeployment workflow-controller argo 
+validateDeployment workflow-controller argo
 echo "Validate for eventsource controller Deployment...."
-validateDeployment eventsource-controller argo-events 
+validateDeployment eventsource-controller argo-events
 echo "Validate for event bus controller Deployment ..."
-validateDeployment eventbus-controller argo-events 
+validateDeployment eventbus-controller argo-events
 echo "Validate for Sensor Controller Deployment..."
-validateDeployment sensor-controller argo-events 
+validateDeployment sensor-controller argo-events
 echo "Validate for Events webhook deployment.."
-validateDeployment events-webhook argo-events 
+validateDeployment events-webhook argo-events
 echo "==========================================================="
 echo "Installation Done for ArgoCD, ArgoEvent and Argo Workflow. We will not use ArgoCD for this Demo."
 echo "==========================================================="
 echo "==========================================================="
 echo "Create Secret with Context, Namespace Workflow, PV and PVC for tf remote"
 echo "==========================================================="
-cd $mydir 
+cd $mydir
 cd prerequisite
 ./createrest.sh
-sleep 5 
+sleep 5
 echo "==========================================================="
 echo "Change Argo workflow & ArgoCD svc to NodePort for UI Access "
 echo "==========================================================="
 echo "Validate if deployment for argo workflow UI is ready...."
 cd $mydir
-validateDeployment argo-server argo 
+validateDeployment argo-server argo
 kubectl patch svc argo-server -n argo -p '{"spec": {"type": "NodePort"}}'
 
-sleep 3 
+sleep 3
 echo "========================"
 echo "Create Webhook Event "
 echo "========================"
@@ -80,7 +80,7 @@ echo "========================"
 echo "Create Sensor with Workflow"
 echo "========================"
 kubectl apply -f demo/hello_world_webhook/hello-world-sensor.yaml
-#Validate if deployment is ready 
+#Validate if deployment is ready
 validateDeployment "hello-world-webhook-sensor-" argo-events
 
 echo "Deployment is ready for sensor with workflow"
@@ -90,10 +90,10 @@ echo "============================================================="
 sleep 30
 echo "Create the Workflow by hitting URL...."
 PORT=`kubectl get svc event-source-eventsource-svc -n argo-events -o=jsonpath='{.spec.ports[0].nodePort}'`
-echo curl -d '{"message":"Bhiya Ram!!"}' -H "Content-Type: application/json" -X POST http://localhost:${PORT}/hello-world 
-curl -d '{"message":"Bhiya Ram!!"}' -H "Content-Type: application/json" -X POST http://localhost:${PORT}/hello-world 
+echo curl -d '{"message":"Bhiya Ram!!"}' -H "Content-Type: application/json" -X POST http://localhost:${PORT}/hello-world
+curl -d '{"message":"Bhiya Ram!!"}' -H "Content-Type: application/json" -X POST http://localhost:${PORT}/hello-world
 echo "List All the workflows..."
-argo list -n workflows 
+argo list -n workflows
 echo "Run following command to get status of workflow listed..."
 echo "argo get <workflow> -n workflows"
 echo "Ex : argo get hello-world-prmcz -n workflows"
